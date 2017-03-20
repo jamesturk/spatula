b6e3cb067446c05a586b6c004204304580380cc8
James Turk <james.p.turk@gmail.com>
2017-03-20 19:03:42 -0400

    0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 from setuptools import setup, find_packages
 
 setup(name="spatula",
-      version='0.0.1',
+      version='0.0.2',
       py_modules=['spatula'],
       author="James Turk",
       author_email='james.p.turk@gmail.com',
