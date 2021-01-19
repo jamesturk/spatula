0cf375a710fd908ace65383dbad9cf5fbffc5213
James Turk <dev@jamesturk.net>
2021-01-18 20:34:55 -0500

    move imports to top level, the API is small and life is short

--- spatula/__init__.py
+++ spatula/__init__.py
@@ -1,1 +1,12 @@
-
+from .pages import (  # noqa
+    Page,
+    HtmlPage,
+    XmlPage,
+    JsonPage,
+    ListPage,
+    CsvListPage,
+    HtmlListPage,
+    JsonListPage,
+)
+from .selectors import SelectorError, Selector, XPath, SimilarLink, CSS  # noqa
+from .core import Workflow, Source, URL, NullSource  # noqa
