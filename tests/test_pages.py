46290cdd33d6c85425fea0bedbb7028a6baf3bc8
James Turk <dev@jamesturk.net>
2021-02-02 23:06:25 -0500

    test list pages

--- tests/test_pages.py
+++ tests/test_pages.py
@@ -1,8 +1,15 @@
 import json
 from dataclasses import dataclass
-from spatula import HtmlPage, XmlPage, JsonPage  # TODO PdfPage
-
-# from spatula import CsvListPage, HtmlListPage, XmlListPage, JsonListPage
+from spatula import (
+    HtmlPage,
+    XmlPage,
+    JsonPage,
+    CsvListPage,
+    HtmlListPage,
+    XmlListPage,
+    JsonListPage,
+    XPath,
+)
 
 SOURCE = "https://example.com"
 
@@ -10,6 +17,10 @@ SOURCE = "https://example.com"
 @dataclass
 class Response:
     content: bytes
+
+    @property
+    def text(self):
+        return self.content
 
     def json(self):
         return json.loads(self.content)
@@ -38,3 +49,41 @@ def test_json_page():
     p.response = Response(json.dumps(nested))
     p.postprocess_response()
     assert p.data == nested
+
+
+def test_csv_list_page():
+    p = CsvListPage(source=SOURCE)
+    p.response = Response("a,b,c\n1,2,3\n4,5,6")
+    p.postprocess_response()
+    data = list(p.process_page())
+    assert len(data) == 2
+    assert data[0] == {"a": "1", "b": "2", "c": "3"}
+
+
+def test_html_list_page():
+    p = HtmlListPage(source=SOURCE)
+    p.selector = XPath("//li/text()")
+    p.response = Response("<ul><li>one</li><li>two</li><li>three</li></ul>")
+    p.postprocess_response()
+    data = list(p.process_page())
+    assert len(data) == 3
+    assert data == ["one", "two", "three"]
+
+
+def test_xml_list_page():
+    p = XmlListPage(source=SOURCE)
+    p.selector = XPath("//item/text()")
+    p.response = Response(
+        "<resp><item>one</item><item>two</item><item>three</item></resp>"
+    )
+    p.postprocess_response()
+    data = list(p.process_page())
+    assert data == ["one", "two", "three"]
+
+
+def test_json_list_page():
+    p = JsonListPage(source=SOURCE)
+    p.response = Response(json.dumps(["one", "two", "three"]))
+    p.postprocess_response()
+    data = list(p.process_page())
+    assert data == ["one", "two", "three"]
